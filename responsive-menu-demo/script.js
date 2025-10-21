0fd562758e42d65a8ca90306ed7e8d387d66de71
anushkaramwani07 <146786899+anushkaramwani07@users.noreply.github.com>
2025-10-20 19:47:17 -0700

    edited js

--- responsive-menu-demo/script.js
+++ responsive-menu-demo/script.js
@@ -1,1 +1,13 @@
 /*JS for responsive menu demo*/
+//This says to wait untill the HTML page is loaded before running JS
+//otherwise, it may try to find burger icon before it exists
+document.addEventListener("DOMContentLoaded", function() {
+
+  //respond to clicks on the burger icon
+
+  //this will find the element named navBurger and listen for clicks
+  document.querySelector("#navBurger").addEventListener("clicked", function(e){
+    document.querySelector("nav").classList.toggle("clicked");
+
+  });
+});
