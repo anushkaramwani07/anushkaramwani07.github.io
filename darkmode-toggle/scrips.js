04bdb0faed518a7277b50dc6c293d103ffeb2338
anushkaramwani07 <146786899+anushkaramwani07@users.noreply.github.com>
2025-10-29 12:48:27 -0700

    linked js

--- darkmode-toggle/scrips.js
+++ darkmode-toggle/scrips.js
@@ -1,10 +1,12 @@
 // adding DOM loading event lister
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-let darkModeButton = document.querySelector("#toggleDarkMode");
-darkModeButton.addEventListener("click", function(e){
-  document.body.classList.toggle("darkMode");
-});
+      let darkModeButton = document.querySelector("#toggleDarkMode");
+      
+      darkModeButton.addEventListener("click", function (e) 
+      {
+            document.body.classList.toggle("darkMode");
+      });
 
 });
