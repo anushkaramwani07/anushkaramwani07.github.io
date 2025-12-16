b01fae76786cf8d68876af2b6eeda5eb29467c3f
anushkaramwani07 <146786899+anushkaramwani07@users.noreply.github.com>
2025-12-16 14:15:42 -0800

    added fnc

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -8,3 +8,18 @@ const pet = {
 
 pet["true"]= "hello";
 console.log(pet["true"]);
+
+
+
+
+
+let myTri = {
+  a:5,
+  b:12,
+  getArea: function(){
+    return (this.a * this.b) / 2;
+  },
+  getHypotenuse: function(){
+    return Math.sqrt((this.a ** 2) + (this.b ** 2));    
+  },
+};
