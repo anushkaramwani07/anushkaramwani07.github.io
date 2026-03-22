23bead58622c184ca66f924776f3e483d0537b53
A <146786899+anushkaramwani07@users.noreply.github.com>
2026-03-22 23:14:11 +0000

    added js

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -1,1 +1,20 @@
-//
+
+
+
+
+
+
+
+//willrun when DOM loads
+document.addEventListener("DOMContentLoaded", function(e)){
+
+    let allCards = document.querySelectorAll(".card");
+    let gameboard = document.querySelector("#gameBoard");
+
+
+    for(x = 0; x<allCards.length; x++){
+        let randNum = Math.floor( Math.random() * allCards.length)
+        gameboard.insertBefore( allCards[x], gameboard.children[randNum]);
+        
+    }
+}
