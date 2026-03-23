6a709cdfa5cd999a16a9869238c7c64267d3a722
A <146786899+anushkaramwani07@users.noreply.github.com>
2026-03-23 01:25:56 +0000

    added more js

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -1,22 +1,45 @@
 
 
-function flipCard() { 
+function flipCard() {
+
     this.classList.add("clicked");
+
+    //this will get all the cards that were clicked
+    let allClickedCards = document.querySelectorAll(".clicked");
+
+    if (allClickedCards.length < 2) {
+        this.classList.add("clicked");
+    }
+
+    allClickedCards = document.querySelectorAll(".clicked");
+
+    if (allClickedCards.length == 2) {
+
+        let card1 = allClickedCards[0].classList.toString();
+        let card2 = allClickedCards[1].classList.toString();
+
+        if (card1 == card2) {
+            console.log("Its a match!!");
+        } else {
+            console.log("Its not a match!!");
+        }
+    }
 }
 
 
 
 
 //willrun when DOM loads
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function (e) {
 
+    //just storing them in this variable
     let allCards = document.querySelectorAll(".card");
     let gameboard = document.querySelector("#gameBoard");
 
 
-    for (x = 0; x<allCards.length; x++){
-        let randNum = Math.floor( Math.random() * allCards.length );
-        gameboard.insertBefore( allCards[x], gameboard.children[randNum]);
+    for (x = 0; x < allCards.length; x++) {
+        let randNum = Math.floor(Math.random() * allCards.length);
+        gameboard.insertBefore(allCards[x], gameboard.children[randNum]);
         allCards[x].addEventListener("click", flipCard);
     }
 });
