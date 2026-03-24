44acaa2827da243783c0b7053a665fb13ec53d43
A <146786899+anushkaramwani07@users.noreply.github.com>
2026-03-24 00:50:46 +0000

    fixed the code

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -1,9 +1,9 @@
 
-let clearClicks = 0;
-
-function clearClicks() {
+function clearClicks() 
+{
     let allClickedCards = document.querySelectorAll(".clicked");
-    for (let eachCard of allClickedCards) {
+    for (let eachCard of allClickedCards) 
+    {
         eachCard.classList.remove("clicked");
     }
     playerClicks++;
@@ -11,43 +11,51 @@ function clearClicks() {
 
     //check for winning
     let allCards = documentquerySelectorAll(".card");
-    let matchedCards = document.querySekectorAll(".matched");
+    let matchedCards = document.querySelectorAll(".matched");
 
-    if (allCards.length == matchedCards.length) {
+    if (allCards.length == matchedCards.length) 
+    {
         //player has matched all cards
         document.querySelctor("#winning").innerHTML = "You Won!"
     }
 }
 
-function flipCard() {
+function flipCard() 
+{
 
-    if (!this.classList.contains("matched")) {
+    if (!this.classList.contains("matched")) 
+    {
 
         //this will get all the cards that were clicked
         let allClickedCards = document.querySelectorAll(".clicked");
 
-        if (allClickedCards.length < 2) {
+        if (allClickedCards.length < 2) 
+        {
             this.classList.add("clicked");
         }
 
         //refresh the click
         allClickedCards = document.querySelectorAll(".clicked");
 
-        if (allClickedCards.length == 2) {
+        if (allClickedCards.length == 2) 
+        {
 
             playerClicks++;
 
             let card1 = allClickedCards[0].classList.toString();
             let card2 = allClickedCards[1].classList.toString();
 
-            if (card1 == card2) {
+            if (card1 == card2) 
+            {
                 console.log("Its a match!!");
                 allClickedCards[0].classList.add("matched");
                 allClickedCards[1].classList.add("matched");
 
-            } else {
+            } 
+            else 
+            {
                 console.log("Its not a match!!");
-                
+
             }
             window.setTimeout(clearClicks, 2000);
         }
@@ -55,17 +63,19 @@ function flipCard() {
 }
 
 //will run when DOM loads
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function (e) 
+{
 
     //just storing them in this variable
     let allCards = document.querySelectorAll(".card");
     let gameboard = document.querySelector("#gameBoard");
 
 
-    for (x = 0; x < allCards.length; x++) {
+    for (x = 0; x < allCards.length; x++) 
+    {
         let randNum = Math.floor(Math.random() * allCards.length);
         gameboard.insertBefore(allCards[x], gameboard.children[randNum]);
-        
+
         allCards[x].addEventListener("click", flipCard);
     }
 });
